285a699bb34ba87c958af1b67c380c915909fad3
Joel Peralta <peraltajoel88@gmail.com>
2024-09-11 08:29:02 -0400

    add theme

---
+++ theme/index.js
@@ -0,0 +1,10 @@
+
+export const theme = {
+    background: "#fff",
+    text: "#fff"
+}
+
+export const styles = {
+    text: {color: theme.text},
+    background: {backgroundColor: theme.background}
+}
